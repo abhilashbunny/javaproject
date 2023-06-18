8d991ecf00be35b1d308997833cd7e32d49df77f
abhilashbunny <abhilashbunny2005@gmail.com>
2023-06-18 15:57:35 -0700

    abhilashbunny

---
+++ src/pg/Hello.java
@@ -0,0 +1,17 @@
+package pg;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int age;
+		double height;
+		
+		age=21;
+		height=5.2;
+	
+			System.out.print(age+"years");
+			System.out.print(height+"meters"); 
+	}
+
+}
---
+++ src/pg/sourabh.java
@@ -0,0 +1,23 @@
+package pg;
+
+public class sourabh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char choice=(A);
+		switch(choice)
+		{
+	case A:System.out.print("exlent");
+	break;
+	case B:System.out.print("good");
+	break;
+	case C:System.out.print("Averge");
+	break;
+	case D:System.out.print("tas");
+	break;
+	default :System.out.print("invoid choice");
+}
+}
+}
+
+
